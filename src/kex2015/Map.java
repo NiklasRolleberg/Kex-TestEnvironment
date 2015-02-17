eb259fb57f1a37a27b53eff7248175e6c1c6efa9
KotzDev <lubomirkotzev@gmail.com>
2015-02-17 16:14:11 +0100

    Some map ideas

--- src/kex2015/Map.java
+++ src/kex2015/Map.java
@@ -1,5 +1,33 @@
 package kex2015;
 
 public class Map {
+	
+	public Map() {
+		// TODO Auto-generated constructor stub
+		//TODO read file generated from MATLAB?
+		
+		
+	}
+	/** Returns sensor data, possibly depth and front sonar in an array? */
+	public double[] getSensorData(double x, double y){
+		double[] a = {0,0};
+		return a;
+	}
+	
+
+	
+	
+	
+	public class MapElement{
+		double depth;
+		boolean isLand;
+		double x;
+		double y;
+		
+		
+		public MapElement() {
+			// TODO Auto-generated constructor stub
+		}
+	}
 
 }
