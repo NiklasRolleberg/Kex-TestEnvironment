83c1c9f60e5e42417dd8bfc10375405923286c9c
NiklasRolleberg <niklasr93@gmail.com>
2015-02-17 15:10:04 +0100

    first

---
+++ src/kex2015/TestEnvironment.java
@@ -0,0 +1,14 @@
+package kex2015;
+
+public class TestEnvironment {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JAG LEVER!");
+
+	}
+
+}
