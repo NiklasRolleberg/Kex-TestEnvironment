15e53007b9c84cc20d51fad624af6e3a32b12f1d
NiklasRolleberg <niklasr93@gmail.com>
2015-02-17 15:34:21 +0100

    båtsaker

--- src/kex2015/Boat.java
+++ src/kex2015/Boat.java
@@ -1,5 +1,45 @@
 package kex2015;
 
-public class Boat {
+public class Boat implements Runnable {
+	
+	private double[] position = {0,0};
+	private double heading = 0;
+	private double speed = 0;
+	
+	private long dt = 100;
+	private boolean stop = false;
+	
+	public Boat(double pauseTime) {
+		//TODO fixa saker
+		
+	}
+	
+	private void updatePos(long dt) {
+		System.out.println("UpdatePosition");
+		this.dt = dt;
+		//TODO Update position of boat
+	}
+	
+	public void stop() {
+		stop = true;
+	}
+
+	@Override
+	public void run() {
+		
+		while(!stop) {
+			
+			System.out.println("JAG ÄR EN BÅT!");
+			
+			try {
+				Thread.sleep(dt);
+			} catch (InterruptedException e) {
+				System.err.println("Fel i Boat: " + e);
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
 
 }
