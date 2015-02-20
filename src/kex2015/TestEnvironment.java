eaa4ef9490b2cfaca336d2b965be3729c2e1f4be
NiklasRolleberg <niklasr93@gmail.com>
2015-02-20 13:06:46 +0100

    nyare view

--- src/kex2015/TestEnvironment.java
+++ src/kex2015/TestEnvironment.java
@@ -41,7 +41,7 @@ public class TestEnvironment {
 		viewThread.start();
 		
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/kex2015/View.java
+++ src/kex2015/View.java
@@ -4,12 +4,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class View extends JFrame implements Runnable {
 	
+	BufferedImage map;
 	Map seaFloor;
 	Boat boat;
 	JPanel image;
@@ -21,10 +23,77 @@ public class View extends JFrame implements Runnable {
 	public View(Map seafloor, Boat boat, long dt) {
 		this.seaFloor = seafloor;
 		this.boat = boat;
-		this.image = new mapView();
 		this.dt = dt;
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		/**Draw a map*/
+		map = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = map.getGraphics();
+		
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.clearRect(0, 0, size, size);
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, size, size);
+		
+		//TODO Ta reda på max/min lat och long
+		double longStart = 0;
+		double longStop = size;
+		double stepLong = (longStop + 0) / size;
+		
+		double latStart = 0;
+		double latStop = size;
+		double stepLat = (longStop + 0) / size;
+		
+		for(int x=0; x < size; x++) {
+			for(int y=0; y < size; y++) {
+				Color color;
+				
+				if(Math.random() < 0.5) {
+					color = Color.CYAN;
+				}
+				else { 
+					color = Color.green;
+				}
+				
+				/*
+				double longitude = longStart + stepLong*x;
+				double latitude = latStart + stepLat*y;
+				
+				double[] data = seafloor.getSensorData(longitude, latitude);
+				double depth = data[0];
+				System.out.println(depth);
+				if(depth < -20) {
+					color = color.BLACK; 
+				}
+				else if(depth < -10)	{
+					color = color.BLUE;
+					color = color.darker();
+				}
+				else if(depth < -5) {
+					color = color.BLUE;
+				}
+				else if(depth < -3) {
+					color = color.BLUE;
+					color = color.brighter();
+				}
+				else if (depth < -0) {
+					color = color.CYAN;
+				}
+				else{
+					color = color.GREEN;
+				}
+				*/
+				g2d.setColor(color);
+				g2d.drawLine(x, y, x, y);
+			}
+		}
+		
+		//g.translate(-100, -100);
+		//g.dispose();
+		
+		
+		
+		this.image = new mapView();
 		this.add(image);
 		pack();
 		setVisible(true);
@@ -58,25 +127,11 @@ public class View extends JFrame implements Runnable {
 		}
 		
 		public void paint(Graphics g) {
-			Graphics2D g2d = (Graphics2D) g;
-			g2d.clearRect(0, 0, size, size);
-			g.setColor(Color.BLUE);
-			g.fillRect(0, 0, size, size);
 			
-			for(int x=0; x < size; x++) {
-				for(int y=0; y < size; y++) {
-					//double depth = map.getDepth(x/realSize,y/realsize); //TODO gör inte så
-					Color color;
-					if(Math.random() < 0.5) {
-						color = Color.CYAN;
-					}
-					else { 
-						color = Color.green;
-					}
-					g2d.setColor(color);
-					g2d.drawLine(x, y, x, y);
-				}
-			}
+			g.drawImage(map, 0, 0, null);
+			Graphics2D g2d = (Graphics2D) g;
+			
+			g2d.drawOval((int) (Math.random() * size), (int) (Math.random() * size), 20, 20);
 			
 		}
 	}
