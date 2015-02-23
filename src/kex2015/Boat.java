510ed55570d1ff3c35e1543ef4e6b6749eb516d6
NiklasRolleberg <niklasr93@gmail.com>
2015-02-23 12:39:45 +0100

    en ny funktion

--- src/kex2015/Boat.java
+++ src/kex2015/Boat.java
@@ -20,6 +20,25 @@ public class Boat implements Runnable {
 		System.out.println("Boat created");
 	}
 	
+	/**
+	 * @return
+	 * 0: latitude
+	 * 1: longitude
+	 * 2: heading
+	 * 3: sonar 1 (depth)
+	 * 4: sonar 2 (forward)
+	 */
+	public double[] getSensordata(double latitude, double longitude) {
+		
+		double[] r = new double[5];
+		r[0] = this.position[0];
+		r[1] = this.position[1];
+		r[2] = this.heading;
+		r[3] = map.getDepth(latitude, longitude);
+		r[4] = 0;
+		return r;
+	}
+	
 	private void updatePos() {
 		System.out.println("UpdatePosition");
 		if(lastUpdate == 0) {
