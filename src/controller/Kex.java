34d9c1f05c003910c8aa2fc8fefde38e2ab8a4eb
KotzDev <lubomirkotzev@gmail.com>
2015-04-17 16:39:50 +0200

    almost drawing the search cell
    blergh

--- src/controller/Kex.java
+++ src/controller/Kex.java
@@ -1,6 +1,20 @@
 package controller;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
 import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import kex2015.Boat;
 
@@ -97,8 +111,8 @@ public class Kex implements Runnable{
 			xMin = minX();
 			yMin = minY();
 			
-			int dx = (int)(maxX()-minX());
-			int dy = (int)(maxY()-minY());
+			int dx = (int)(Math.round(maxX())-Math.round(minX()));
+			int dy = (int)(Math.round(maxY())-Math.round(minY()));
 			
 			System.out.println("max x:" + maxX() + " min x:" + minX()+ " dx: " + dx);
 			System.out.println("max y:" + maxY() + " min y:" + minY()+ " dy: " + dy);
@@ -110,13 +124,13 @@ public class Kex implements Runnable{
 			//make up some status codes: 99 is oob, 0 is not yet scanned, 1 is scanned water, 2 is land/unreachable, more?!
 			//mark the ones that are out of bounds: 
 			//visualise the results! (not in here obviously)
-			cellMatrix = new searchElement[dx+1][dy+1];
+			cellMatrix = new searchElement[dx][dy];
 			//index for the array
 			int ix = 0; 
 			int iy = 0;
 			double xLeft, xRight;
 			boolean readCellIntoMemory;
-			readCellIntoMemory = false;
+			readCellIntoMemory = true;
 			if (readCellIntoMemory){
 				for (int y=(int)Math.round(yMin); y<(int)Math.round(yMax); y++){
 					for (int x=(int)Math.round(xMin); x <(int)Math.round(xMax);x++){
@@ -133,8 +147,25 @@ public class Kex implements Runnable{
 					iy++;
 					ix = 0;
 				}
+				
 			}
+			drawMatrix testDraw = new drawMatrix(this);
+			//System.out.println("----------cell read done!");
+
+			//check ALL the elements!
+			/*for (searchElement[] ea : cellMatrix){
+				for (searchElement e : ea){
+					if (e.status ==  99){
+						System.out.println("wat");
+					}
+				}
+			}
+			System.out.println("check done!");*/
+			
+			
 		}
+		
+		
 		public double findX(double y, boolean right) {
 			int[] l1 = {-1,-1};
 			int[] l2 = {-1,-1};
@@ -142,8 +173,8 @@ public class Kex implements Runnable{
 			int l = ypos.size();
 			for(int i=0; i < l; i++) {
 				
-				if(((ypos.get((i+1)%l) > y) && (ypos.get(i) < y))
-				|| ((ypos.get((i+1)%l) < y) && (ypos.get(i) > y))) {
+				if(((ypos.get((i+1)%l) >= y) && (ypos.get(i) <= y))
+				|| ((ypos.get((i+1)%l) <= y) && (ypos.get(i) >= y))) {
 					//interpolate
 					if(l1[0] == -1) {
 						l1[0] = (i+1)%l;
@@ -160,7 +191,7 @@ public class Kex implements Runnable{
 			}
 			
 			/*error return some default value*/
-			if(l1[0] == -1 || l1[1] == -1 || l2[0] == -1 || l2[1] == 0) {
+			if(l1[0] == -1 || l1[1] == -1 || l2[0] == -1 || l2[1] == -1) {
 				double meanX = 0;
 				for(int i=0;i<xpos.size();i++) {
 					meanX += xpos.get(i);
@@ -246,10 +277,58 @@ public class Kex implements Runnable{
 		double xCoord;
 		double yCoord;
 		int status;
-		private searchElement(double x, double y, int s){
+		public searchElement(double x, double y, int s){
 			xCoord = x;
 			yCoord = y;
 			status = s;
+		}
+	}
+	
+	private class drawMatrix extends JPanel{
+		SearchCell myCell;
+		JFrame myFrame;
+		
+		public drawMatrix(SearchCell inCell){
+			myFrame = new JFrame();
+			myFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+			myFrame.setPreferredSize(new Dimension(500, 500));
+			myFrame.add(this);
+			myFrame.pack();
+			myFrame.setVisible(true);
+			myCell = inCell;
+			this.repaint();
+			
+		}
+		
+		@Override
+		public void paint(Graphics g){
+		
+			Graphics2D g2d = (Graphics2D) g;
+			//draw elements 
+			for (searchElement[] ea : myCell.cellMatrix){
+				for (searchElement e : ea){
+					
+					if (e.status ==  99){
+						System.out.println("wat");
+					}
+				}
+			}
+			
+			//draw polygon
+			for(int i=0; i<myCell.xpos.size()-1; i++) {
+				int x0 = polygonX.get(i).intValue();
+				int y0 = polygonY.get(i).intValue();
+				
+				int x1 = polygonX.get(i+1).intValue();
+				int y1 = polygonY.get(i+1).intValue();
+				g.setColor(Color.RED);
+				g.drawLine(x0, y0, x1, y1);
+			}
+			g.drawLine(polygonX.get(polygonX.size()-1).intValue(), (polygonY.get(polygonX.size()-1).intValue())
+									,polygonX.get(0).intValue(), polygonY.get(0).intValue());
+			
+			
+			
 		}
 	}
 	
